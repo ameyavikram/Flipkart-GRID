2a46eb94b4d94fcd0e0b384859483cd77755350a
ameyavikram <ameya.vikram15@gmail.com>
2019-02-09 18:27:20 +0530

    changes some stuff to check

--- Pytorch YOLO/utils/utils.py
+++ Pytorch YOLO/utils/utils.py
@@ -202,8 +202,14 @@ def build_targets(
 
     nGT = 0
     nCorrect = 0
+
+    print("target shape:", target.shape, "nB:", nB)
+
     for b in range(nB):
         for t in range(target.shape[1]):
+
+            print('b:', b, ' t:' , t)
+
             if target[b, t].sum() == 0:
                 continue
             nGT += 1

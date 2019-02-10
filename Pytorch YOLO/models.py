9f93fd2cb26fba25dfb8071cb6fa368b24693060
ameyavikram <ameya.vikram15@gmail.com>
2019-02-10 21:28:10 +0530

    changed some stuff to check

--- Pytorch YOLO/models.py
+++ Pytorch YOLO/models.py
@@ -177,7 +177,7 @@ class YOLOLayer(nn.Module):
 
             nProposals = int((pred_conf > 0.5).sum().item())
             recall = float(nCorrect / nGT) if nGT else 1
-            precision = float(nCorrect / nProposals)
+            precision = float(nCorrect / max(nProposals,1))
 
             # Handle masks
             mask = Variable(mask.type(ByteTensor))
